paradoxical-dev/zeal.nvim

commit e27c7ec54842d6add1d6dab9eae24f9936650429

fix(browser): add handling for docset link fragment
This allows the plugin to link directly to the document. For example,
without this fix, searching 'require' in the Lua 5.5 docs will bring you
to the very top of the reference manual.

lua/zeal/docsets.lua
@@ -45,21 +45,26 @@ end
 ---@return table
 function M.entries(docset)
 	local db = docset.path .. "/Contents/Resources/docSet.dsidx"
-	local raw =
-		vim.fn.systemlist(string.format("sqlite3 '%s' \"SELECT name, path FROM searchIndex ORDER BY name\"", db))
+	local raw = vim.fn.systemlist(
+		string.format("sqlite3 '%s' \"SELECT name, path, fragment FROM searchIndex ORDER BY name\"", db)
+	)
 	local entries = {}
 
 	for _, line in ipairs(raw) do
-		local entry_name, path = line:match("^(.-)|(.+)$")
+		local entry_name, path, fragment = line:match("^(.-)|(.-)|(.*)$")
 		if entry_name and path then
 			-- strip path metadata?
 			-- local stripped = path:match(">([^>]+)$") or path
 			local stripped = path:match("^.*>(.+)$") or path
 			-- local filepath = stripped:match("^([^#]+)")
+			local full_path = docset.path .. "/Contents/Resources/Documents/" .. stripped
+			if fragment and fragment ~= "" then
+				full_path = full_path .. "#" .. fragment
+			end
 
 			table.insert(entries, {
 				display = entry_name,
-				path = docset.path .. "/Contents/Resources/Documents/" .. stripped,
+				path = full_path,
 			})
 		end
 	end

commit 57d0485a58b412523aae40884cc56c4989a614c5

fix: Docsets without a "fragment" columns
Some docsets (notably Bash) have no "fragment" column. That results in
"zeal.nvim: No entries for …". We need to deal with the lack of that
column.

To fix this, add a fallback to just SELECT name, path.

Furthermore, switch to vim.system and the json output mode in sqlite3
for better error and special characters (the pipe column separator
symbol) handling.

Fixes: e27c7ec54842 ("fix(browser): add handling for docset link fragment")
Fixes: https://github.com/paradoxical-dev/zeal.nvim/issues/4

lua/zeal/docsets.lua
@@ -45,28 +45,39 @@ end
 ---@return table
 function M.entries(docset)
 	local db = docset.path .. "/Contents/Resources/docSet.dsidx"
-	local raw = vim.fn.systemlist(
-		string.format("sqlite3 '%s' \"SELECT name, path, fragment FROM searchIndex ORDER BY name\"", db)
-	)
-	local entries = {}
 
-	for _, line in ipairs(raw) do
-		local entry_name, path, fragment = line:match("^(.-)|(.-)|(.*)$")
-		if entry_name and path then
-			-- strip path metadata?
-			-- local stripped = path:match(">([^>]+)$") or path
-			local stripped = path:match("^.*>(.+)$") or path
-			-- local filepath = stripped:match("^([^#]+)")
-			local full_path = docset.path .. "/Contents/Resources/Documents/" .. stripped
-			if fragment and fragment ~= "" then
-				full_path = full_path .. "#" .. fragment
-			end
-
-			table.insert(entries, {
-				display = entry_name,
-				path = full_path,
-			})
+	local result = vim.system({
+		"sqlite3", "-json", db,
+		"SELECT name, path, fragment FROM searchIndex ORDER BY name",
+	}, { text = true }):wait()
+	if result.code ~= 0 then
+		result = vim.system({
+			"sqlite3", "-json", db,
+			"SELECT name, path FROM searchIndex ORDER BY name",
+		}, { text = true }):wait()
+		if result.code ~= 0 then
+			vim.notify("zeal.nvim: sqlite error: " .. result.stderr, vim.log.levels.ERROR)
+			return {}
 		end
+	end
+
+
+	local entries = {}
+	local rows = vim.json.decode(result.stdout, { luanil = { object = true, array = true } })
+	for _, row in ipairs(rows) do
+		-- strip path metadata?
+		-- local stripped = row.path:match(">([^>]+)$") or row.path
+		local stripped = row.path:match("^.*>(.+)$") or row.path
+		-- local filepath = stripped:match("^([^#]+)")
+		local full_path = docset.path .. "/Contents/Resources/Documents/" .. stripped
+		if row.fragment and row.fragment ~= "" then
+			full_path = full_path .. "#" .. row.fragment
+		end
+
+		table.insert(entries, {
+			display = row.name,
+			path = full_path,
+		})
 	end
 	return entries
 end